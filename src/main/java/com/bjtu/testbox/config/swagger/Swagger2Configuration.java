f9d4714dc7e6da1cc504745a5e7531b13b1d7aea
ztz <874102849@qq.com>
2020-08-01 23:00:24 +0800

    暂时禁用swagger2,shiro

--- src/main/java/com/bjtu/testbox/config/swagger/Swagger2Configuration.java
+++ src/main/java/com/bjtu/testbox/config/swagger/Swagger2Configuration.java
@@ -11,11 +11,11 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import  springfox.documentation.swagger2.annotations.EnableSwagger2;
 //swagger2的配置文件，在项目的启动类的同级文件建立
-@Configuration
-@EnableSwagger2
+//@Configuration
+//@EnableSwagger2
 //是否开启swagger，正式环境一般是需要关闭的（避免不必要的漏洞暴露！），
 // 可根据springboot的多环境配置进行设置
-@ConditionalOnProperty(name = "swagger.enable",  havingValue = "true")
+//@ConditionalOnProperty(name = "swagger.enable",  havingValue = "true")
 public class Swagger2Configuration {
      // swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
 
